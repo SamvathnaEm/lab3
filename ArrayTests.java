1d2a73f93f50b6a181b7c9502d5c9aa48746146a
Eun_Bee <emsamvathana@gmail.com>
2023-10-23 17:50:17 -0700

    Updated

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,10 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+    @Test
+  public void testReversed1() {
+    int[] input1 = { 1,2,3,4};
+    assertArrayEquals(new int[]{ 4,3,2,1}, ArrayExamples.reversed(input1));
+  }
 }
